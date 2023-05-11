2acdf66dbc71b7e604aed140ed4fe05a3c0ff4b7
Robbie Prokop <robbieprokop@gmail.com>
2023-05-10 18:24:18 -0600

    begin creating chess board

---
+++ frontend/src/components/Board.tsx
@@ -0,0 +1,82 @@
+import "../styles/board.scss";
+
+const Board = () => {
+  return (
+    <div className="board">
+      <ul>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+        <li className="square dark"></li>
+        <li className="square light"></li>
+      </ul>
+    </div>
+  );
+};
+export default Board;
