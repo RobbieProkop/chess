c65579fd7f5c2e2a6f3dea3265c41664ba047725
Robbie Prokop <robbieprokop@gmail.com>
2023-05-11 01:08:45 -0600

    start working with react data gui first

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Chessboard from "./components/Board";
 
 function App() {
+  const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  const ranks = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <div className="App">
-      <Chessboard />
+      <Chessboard ranks={ranks} files={files} />
     </div>
   );
 }
--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,110 +1,108 @@
 import "../styles/board.scss";
 
-const Chessboard = () => {
-  const pieces = {
-    empty: 0,
-    wP: 1,
-    wN: 2,
-    wB: 3,
-    wR: 4,
-    wQ: 5,
-    wK: 6,
-    bP: 7,
-    bN: 8,
-    bB: 9,
-    bR: 10,
-    bQ: 11,
-    bK: 12,
-  };
+type Props = {
+  files: string[];
+  ranks: number[];
+};
 
-  const boardSquares = 120;
+const Chessboard = ({ ranks, files }: Props) => {
+  // const pieces = {
+  //   empty: 0,
+  //   wP: 1,
+  //   wN: 2,
+  //   wB: 3,
+  //   wR: 4,
+  //   wQ: 5,
+  //   wK: 6,
+  //   bP: 7,
+  //   bN: 8,
+  //   bB: 9,
+  //   bR: 10,
+  //   bQ: 11,
+  //   bK: 12,
+  // };
 
-  const files = {
-    fA: 0,
-    fB: 1,
-    fC: 2,
-    fD: 3,
-    fE: 4,
-    fF: 5,
-    fG: 6,
-    fH: 7,
-    fNone: 8,
-  };
+  // const boardSquares = 120;
 
-  const ranks = {
-    r1: 0,
-    r2: 1,
-    r3: 2,
-    r4: 3,
-    r5: 4,
-    r6: 5,
-    r7: 6,
-    r8: 7,
-    rNone: 8,
-  };
+  // const files = {
+  //   fA: 0,
+  //   fB: 1,
+  //   fC: 2,
+  //   fD: 3,
+  //   fE: 4,
+  //   fF: 5,
+  //   fG: 6,
+  //   fH: 7,
+  //   fNone: 8,
+  // };
 
-  const colours = { white: 0, black: 1, both: 2 };
-  //side ^=1 || side = side ^1;
+  // const ranks = {
+  //   r1: 0,
+  //   r2: 1,
+  //   r3: 2,
+  //   r4: 3,
+  //   r5: 4,
+  //   r6: 5,
+  //   r7: 6,
+  //   r8: 7,
+  //   rNone: 8,
+  // };
 
-  const squares = {
-    A1: 21,
-    B1: 22,
-    C1: 23,
-    D1: 24,
-    E1: 25,
-    F1: 26,
-    G1: 27,
-    H1: 28,
-    A8: 91,
-    B8: 92,
-    C8: 93,
-    D8: 94,
-    E8: 95,
-    F8: 96,
-    G8: 97,
-    H8: 98,
-    noSq: 99,
-    offBoard: 100,
-  };
+  // const colours = { white: 0, black: 1, both: 2 };
+  // //side ^=1 || side = side ^1;
 
-  const filesBoard = new Array(boardSquares);
-  const ranksBoard = new Array(boardSquares);
+  // const squares = {
+  //   A1: 21,
+  //   B1: 22,
+  //   C1: 23,
+  //   D1: 24,
+  //   E1: 25,
+  //   F1: 26,
+  //   G1: 27,
+  //   H1: 28,
+  //   A8: 91,
+  //   B8: 92,
+  //   C8: 93,
+  //   D8: 94,
+  //   E8: 95,
+  //   F8: 96,
+  //   G8: 97,
+  //   H8: 98,
+  //   noSq: 99,
+  //   offBoard: 100,
+  // };
 
-  //for a given file and rank, we need to return the board square
-  const fileRankToSquare = (file: number, rank: number) => {
-    return 21 + file + rank * 10;
-  };
+  // const filesBoard = new Array(boardSquares);
+  // const ranksBoard = new Array(boardSquares);
 
-  const initFilesRanksBoard = () => {
-    let sq = squares.A1;
+  // //for a given file and rank, we need to return the board square
+  // const fileRankToSquare = (file: number, rank: number) => {
+  //   return 21 + file + rank * 10;
+  // };
 
-    //sets everything to offboard
-    for (let i = 0; i < boardSquares; i++) {
-      filesBoard[i] = squares.offBoard;
-      ranksBoard[i] = squares.offBoard;
-    }
+  // const initFilesRanksBoard = () => {
+  //   let sq = squares.A1;
 
-    //loops through files and ranks, get board square, set filesBoard and ranksBoard to appropriate value
-    for (let rank = 0; rank <= ranks.r8; rank++) {
-      for (let file = files.fA; file <= files.fH; file++) {
-        sq = fileRankToSquare(file, rank);
-        filesBoard[sq] = file;
-        ranksBoard[sq] = rank;
-      }
-    }
+  //   //sets everything to offboard
+  //   for (let i = 0; i < boardSquares; i++) {
+  //     filesBoard[i] = squares.offBoard;
+  //     ranksBoard[i] = squares.offBoard;
+  //   }
 
-    console.log("filesBoard[0]", filesBoard[0]);
-    console.log("ranksBoard[0]", ranksBoard[0]);
-    console.log("filesBoard[squares.A1]", filesBoard[squares.A1]);
-    console.log("ranksBoard[squares.A1]", ranksBoard[squares.A1]);
-    console.log("filesBoard[squares.E8]", filesBoard[squares.E8]);
-    console.log("ranksBoard[squares.E8]", ranksBoard[squares.E8]);
-  };
-  // initFilesRanksBoard();
+  //   //loops through files and ranks, get board square, set filesBoard and ranksBoard to appropriate value
+  //   for (let rank = 0; rank <= ranks.r8; rank++) {
+  //     for (let file = files.fA; file <= files.fH; file++) {
+  //       sq = fileRankToSquare(file, rank);
+  //       filesBoard[sq] = file;
+  //       ranksBoard[sq] = rank;
+  //     }
+  //   }
+  // };
+  // // initFilesRanksBoard();
 
   let board = [];
-  for (let rank = ranks.r1; rank <= ranks.r8; rank++) {
-    for (let file = files.fA; file <= files.fH; file++) {
+  for (let i = 0; i <= files.length; i++) {
+    for (let j = 0; j <= ranks.length; j++) {
       board.push(<span></span>);
     }
   }
